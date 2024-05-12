03acc8edb6b572ed4deb1f59055f887f39b72144
eric anthony <ericanthonywu89@gmail.com>
2024-05-12 11:17:57 +0700

    Update error handling in Axios instance
    This commit improves the error handling in the Axios instance configuration. It now properly handles scenarios where the server provides no response, as well as handles the errors by rejecting the promise with the appropriate error or response data.

--- config/axios/instance.js
+++ config/axios/instance.js
@@ -9,8 +9,10 @@ const instance = axios.create({
 instance.interceptors.response.use((data) => data.data, (error) => {
     if (!error.response) {
         console.error(`${rTracer.id()} server responded with no response`)
+        return Promise.reject(error)
     } else {
         console.error(`${rTracer.id()} Error when doing http request with status code ${error.response.status} \n [${error.config.method}] ${error.config.url} \n headers: ${error.config.headers} \n response: ${error.response.data}`)
+        return Promise.reject(error.response.data)
     }
 })
 
